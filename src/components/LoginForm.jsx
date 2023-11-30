58049ab5f0f93cec51acfab1ff72779850b2d61a
AlejandroNicolasValenti <nicolaspiu12@gmail.com>
2023-11-30 19:36:17 -0300

    cambios en loginForm

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,10 +1,9 @@
-import styles from "../styles/AuthForm.module.css";
-
 import { useId, useRef } from "react";
 import { API_URL } from "../utils/consts";
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 import { useNavigate } from "react-router-dom";
+import styles from "../styles/LoginForm.module.css";
 
 function LoginForm() {
   const ref = useRef(null);
@@ -51,31 +50,33 @@ function LoginForm() {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit} ref={ref} className={styles.form}>
-        <div className={styles.inputGroup}>
-          <label htmlFor={emailRef}>Email:</label>
-          <input
-            type="email"
-            placeholder="my-email@email.com"
-            name="email"
-            id={emailRef}
-          />
-        </div>
+    <div className={styles.loginFormContainer}>
+      <div className={styles.formContainer}>
+        <h2>Iniciar sesión:</h2>
+        <form onSubmit={handleSubmit} ref={ref} className={styles.form}>
+          <div className={styles.inputGroup}>
+            <label htmlFor={emailRef}>Correo electrónico:</label>
+            <input
+              type="email"
+              placeholder="ejemplo@email.com"
+              name="email"
+              id={emailRef}
+            />
+          </div>
 
-        <div className={styles.inputGroup}>
-          <label htmlFor={passwordRef}>Password:</label>
-          <input
-            type="password"
-            placeholder="*******"
-            name="password"
-            id={passwordRef}
-          />
-        </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor={passwordRef}>Contraseña:</label>
+            <input
+              type="password"
+              placeholder="*******"
+              name="password"
+              id={passwordRef}
+            />
+          </div>
 
-        <button>Login</button>
-      </form>
+          <button>Login</button>
+        </form>
+      </div>
     </div>
   );
 }
