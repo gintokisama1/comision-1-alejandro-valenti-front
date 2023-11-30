726bea20c89a3536d28e0189b4aa78d9d7e7a42e
AlejandroNicolasValenti <nicolaspiu12@gmail.com>
2023-11-30 19:09:10 -0300

    estilización del form 2

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,6 +1,7 @@
 import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { API_URL } from "../utils/consts";
+import styles from "../styles/AuthForm.module.css";
 
 function RegisterForm() {
   const ref = useRef(null);
@@ -38,63 +39,65 @@ function RegisterForm() {
   };
 
   return (
-    <div className="container mt-5">
-      <div className="card">
-        <div className="card-body">
-          <h2 className="mb-4">Registro de usuarios</h2>
-          <form onSubmit={handleSubmit} ref={ref}>
-            <div className="mb-3">
-              <label htmlFor="avatar" className="form-label">
-                Avatar
-              </label>
-              <input
-                type="url"
-                className="form-control"
-                id="avatar"
-                placeholder="www.my-avatar.com"
-                name="avatar"
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="email" className="form-label">
-                Correo Electrónico
-              </label>
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                placeholder="ejemplo@email.com"
-                name="email"
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="username" className="form-label">
-                Nombre de Usuario
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="username"
-                placeholder="Joe Doe"
-                name="username"
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="password" className="form-label">
-                Contraseña
-              </label>
-              <input
-                type="password"
-                className="form-control"
-                id="password"
-                placeholder="*******"
-                name="password"
-              />
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Registrar
-            </button>
-          </form>
+    <div className={styles.registerFormContainer}>
+      <div className={styles.formContainer}>
+        <div className="card">
+          <div className="card-body">
+            <h2 className="mb-4">Registro de usuarios</h2>
+            <form onSubmit={handleSubmit} ref={ref}>
+              <div className="mb-3">
+                <label htmlFor="avatar" className="form-label">
+                  Avatar
+                </label>
+                <input
+                  type="url"
+                  className="form-control"
+                  id="avatar"
+                  placeholder="https://www.mi-avatar.com"
+                  name="avatar"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  Correo Electrónico
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="ejemplo@email.com"
+                  name="email"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="username" className="form-label">
+                  Nombre de Usuario
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="username"
+                  placeholder="Joe Doe"
+                  name="username"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">
+                  Contraseña
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder="*******"
+                  name="password"
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Registrar
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
